ae2e878a3d615ff66844d574df00d3a8c1ba5a00
nycheng <42791414+nycheng@users.noreply.github.com>
2018-08-29 21:55:57 -0500

    init

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,11 +13,13 @@ TIMEZONE = 'America/Chicago'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
